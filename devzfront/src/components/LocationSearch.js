74e7dc9c9fd05cf769bc5d135bcb0342cde24651
yossaj <yossaj@icloud.com>
2019-07-06 17:48:35 +0100

    search and location functionality added

--- devzfront/src/components/LocationSearch.js
+++ devzfront/src/components/LocationSearch.js
@@ -2,12 +2,42 @@ import React, {Component} from 'react';
 
 class LocationSearch extends Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchLocation: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ searchLocation: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render (){
 
     return(
       <div className="active-cyan-3 active-cyan-4 mb-4 col-md-6">
-        <input class="form-control-lg" type="text" placeholder="Location" aria-label="Search"/>
-			</div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="form-control-lg"
+            type="text"
+            placeholder="Location"
+            aria-label="Search"
+            name="searchLocation"
+            onChange={this.handleChange}
+            value={this.state.searchLocation}
+          />
+        </form>
+      </div>
     )
   }
 
--- devzfront/src/components/SkillSearch.js
+++ devzfront/src/components/SkillSearch.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
 
-class SkillSearch extends Component{
+class SkillSearch extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      searchedSkill: ""
+      searchSkill: ""
     };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ searchSkill: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render (){
 
     return(
       <div className="active-cyan-3 active-cyan-4 mb-4 col-md-6">
-        <input 
-          className ="form-control-lg" 
-          type="text" 
-          placeholder="Skills" 
-          aria-label="Search"
-          />
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            className ="form-control-lg" 
+            type="text" 
+            placeholder="Skills" 
+            aria-label="Search"
+            name = "searchSkill"
+            onChange={this.handleChange}
+            value = {this.state.searchSkill}
+            />
+        </form>
 			</div>
     )
   }
