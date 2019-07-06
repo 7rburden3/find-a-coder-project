1787c1a708c9dda59f72d83ef1ed1780ca975792
yossaj <yossaj@icloud.com>
2019-07-06 14:01:43 +0100

    bootstrap installed

--- devzfront/src/components/NavBar.js
+++ devzfront/src/components/NavBar.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class NavBar extends Component{
 
   render (){
 
     return(
-      <h2>Nav Bar</h2>
+      <Fragment>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light static-top">
+          <div className="container">
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">Home
+                  <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="dev.html">Tutorials</a>
+                  </li>
+              </ul>
+            </div>
+          </div>
+	      </nav>
+        
+      </Fragment>
+     
     )
   }
 
