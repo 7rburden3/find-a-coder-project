9f9a2257155c1eaae37bedf4b2575d62ac059e28
malkyx <malky.burns@gmail.com>
2019-07-10 14:52:30 +0100

    project delete button working

--- devzfront/src/components/ProjectList.js
+++ devzfront/src/components/ProjectList.js
@@ -1,17 +1,41 @@
 import React from 'react';
 
 const ProjectList = (props) => {
+
+// PROJECT LIST WITH DUMMY DELETE BUTTON
+//     const projects = props.allProjects.map((project, index) =>{
+//    return (
+//        <div className="project-list-item" key={index}>
+//             <li>
+//                <p>{project.projectName}
+//                    <button id="delete-project">Delete</button>
+//                 </p>
+//             </li>
+//         </div>
+//     )
+//     })
+
+// DevzContainer passes deleteProject as the property onProjectDelete to/on ProjectList
+  // new function to handle button click to delete project
+  function handleProjectDelete(event) {
+    const projectId = parseInt(event.target.value);
+    props.onProjectDelete(projectId);
+  };
+
+// PROJECT LIST WITH NEW DELETE BUTTON
+//   // <button className="delete-button" onClick={handleProjectDelete(index)}>Delete</button>
     const projects = props.allProjects.map((project, index) =>{
    return (
        <div className="project-list-item" key={index}>
             <li>
                <p>{project.projectName}
-                   <button id="delete-project">Delete</button>
+               <button className="delete-button" value={project.id} onClick={handleProjectDelete}>Delete</button>
                 </p>
             </li>
         </div>
     )
     })
+
 
         return (
             <div className="container">
