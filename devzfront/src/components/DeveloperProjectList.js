2007ec92396ed12a1662faac7d956d83f7bb2ed9
Ralph Burden <ralphburden@hotmail.com>
2019-07-10 14:16:40 +0100

    add projects listed to developers

---
+++ devzfront/src/components/DeveloperProjectList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const DeveloperProjectList = (props) => {
+  console.log(props);
+  const projects = props.developerProjects.map((projects, index) => {
+    return (
+      <div className = "developer-project" key={index}>
+              <li className="developerproject-li">
+                {projects.projectName}
+              </li>
+            </div>
+    )
+  })
+
+  return (
+    <ul className="developerprojects-ul">
+    {projects}
+    </ul>
+
+  )
+}
+
+export default DeveloperProjectList;
--- devzfront/src/components/ProfileDetail.js
+++ devzfront/src/components/ProfileDetail.js
@@ -2,22 +2,25 @@ import React, { Component, Fragment } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DeveloperSkillList from '../components/DeveloperSkillList';
+import DeveloperProjectList from '../components/DeveloperProjectList';
 
 
 class ProfileDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            developerSkills: []
+            developerSkills: [],
+            developerProjects: []
         }
-        
+
         this.handleDeleteClick = this.handleDeleteClick.bind(this)
         this.handleUpdateClick = this.handleUpdateClick.bind(this)
         this.findDeveloperSkills = this.findDeveloperSkills.bind(this)
+        this.findDeveloperProjects = this.findDeveloperProjects.bind(this)
     }
 
     handleUpdateClick() {
-        console.log("Hello", this.props) 
+        console.log("Hello", this.props)
         this.props.getDetails(this.props.profileDetails)
     }
 
@@ -31,6 +34,7 @@ class ProfileDetail extends Component {
 
     componentDidMount() {
         this.findDeveloperSkills(this.props.profileDetails.id)
+        this.findDeveloperProjects(this.props.profileDetails.id)
     }
 
     findDeveloperSkills(id) {
@@ -47,7 +51,21 @@ class ProfileDetail extends Component {
             });
     }
 
-  
+    findDeveloperProjects(id) {
+        fetch(`http://localhost:8080/developers/${this.props.profileDetails.id}/projects`)
+            .then(res => res.json())
+            .then((data) => {
+                const newData = data._embedded.projects
+                const promises = newData
+                Promise.all(promises)
+                    .then((results) => {
+                        this.setState({ developerProjects: results });
+                        console.log(this.state.developerProjects);
+                    });
+            });
+    }
+
+
 
     render() {
         console.log(this.props.profileDetails)
@@ -76,11 +94,17 @@ class ProfileDetail extends Component {
                                 <ul className="list-group list-group-flush skills">
                                     <DeveloperSkillList developerSkills={this.state.developerSkills} />
                                 </ul>
-                                
+
+                                <h1 className="">Projects</h1>
+                                <hr />
+                                <ul className="list-group list-group-flush projects">
+                                    <DeveloperProjectList developerProjects={this.state.developerProjects} />
+                                </ul>
+
                             </div>
                             <div className= "col-md-6" >
                                 <h3>Who is {developer.firstName}?</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </div>
                         </div>
                     </div>
