91512b50eecdf1f7dfca7387ed8527bc0a65e58d
yossaj <yossaj@icloud.com>
2019-07-10 13:30:51 +0100

    added about me

--- devzfront/src/components/ProfileDetail.js
+++ devzfront/src/components/ProfileDetail.js
@@ -58,6 +58,10 @@ class ProfileDetail extends Component {
                                     <li><h3>Python</h3></li>
                                 </ul>
                             </div>
+                            <div className= "col-md-6" >
+                                <h3>Who is {developer.firstName}?</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+                            </div>
                         </div>
                     </div>
                 </section>
